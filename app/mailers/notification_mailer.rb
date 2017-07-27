a2d7d42fcc0b83a4eabfdfec043259d28398587d
Gretchen <gretchonian@hotmail.com>
2017-07-27 20:59:28 +0000

    Bug Fix

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@otbbbq.com"
 
   def rfq_submitted(rfq)
+    @rfq = rfq
     mail(to: "gretchen.testing.emails@gmail.com",
       subject: "A new RFQ has been submitted from #{rfq.name}!")
   end
