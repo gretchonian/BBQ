7a36d904763ce1d54dc36470a882ef3fe72b7998
Gretchen <gretchonian@hotmail.com>
2017-12-03 18:20:00 +0000

    updated emailing

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,7 +5,7 @@ class NotificationMailer < ApplicationMailer
     @rfq = rfq
     @meats = rfq.foods_ordered(rfq.meats).join(', ')
     @sides = rfq.foods_ordered(rfq.sides).join(', ')
-    mail(to: "gretchen.testing.emails@gmail.com",
+    mail(to: "offthebonebbq1@gmail.com",
       subject: "A new RFQ has been submitted from #{rfq.name}!")
   end
 
