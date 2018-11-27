e84967866659fae088e62913b196646691f9e44d
gretchonian <gretchonian@hotmail.com>
2018-11-26 18:34:48 -0800

    fix rfq update

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,8 +3,8 @@ class FoodsController < ApplicationController
   def update
     current_meat = Food.find(params[:id])
     current_meat.update_attributes(food_params)
-    # render :json
-    # redirect_to rfq_path(current_meat.rfq)
+    render :json
+    redirect_to rfq_path(current_meat.rfq)
   end
 
   private
