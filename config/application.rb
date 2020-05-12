0b92ed28048057ea2db251fcec13fa143c506100
gretchonian <gretchonian@hotmail.com>
2020-05-12 16:13:45 -0700

    coffee-rails update

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Bbq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.i18n.fallbacks = true
     #add fonts path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
